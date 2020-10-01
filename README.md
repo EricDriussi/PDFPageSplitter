2fa910147af933e2c5ca3dcde54a9816442bd3d0
Eric Driussi <ericdriussi@gmail.com>
2020-10-01 09:27:23 +0100

    update

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 ```sh
  pdftk PDFNAME.pdf burst
 ```
+pdfinfo pg_0001.pdf
 
 ```sh
 script.sh
@@ -16,20 +17,3 @@ script.sh
   pdftk `cat fl`  cat output newfile.pdf 
 
 ```
-
-
-----------
-#!/bin/bash
-
-  width=$1
-  height=$2
-  halfWidth=$(( width / 2 ))
-  w2px=$(( halfWidth*10 )) #No clue
-  w3=$(( halfWidth+18 )) #No clue, why 18?
-  hpx=$((  height*10 ))
-  for f in  pg_[0-9]*.pdf ; do
-   lf=left_$f
-   rf=right_$f
-   gs -o ${lf} -sDEVICE=pdfwrite -g${w2px}x${hpx} -c "<</PageOffset [0 0]>> setpagedevice" -f ${f} #[]needs tinkering
-   gs -o ${rf} -sDEVICE=pdfwrite -g${w2px}x${hpx} -c "<</PageOffset [-${w3} 0]>> setpagedevice" -f ${f} #[]needs tinkering
-  done
--- script.sh
+++ script.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-  width=$1
-  height=$2
+  width=783
+  height=559
   halfWidth=$(( width / 2 ))
-  w2px=$(( halfWidth*10 )) #No clue
-  w3=$(( halfWidth+18 )) #No clue, why 18?
+  w2px=$(( halfWidth*10 )) 
   hpx=$((  height*10 ))
+
+#left page start point
+  lx=0
+  ly=0
+#right page start point
+  rx=$(( halfWidth+0 ))
+  ry=0
+
   for f in  pg_[0-9]*.pdf ; do
    lf=left_$f
    rf=right_$f
-   gs -o ${lf} -sDEVICE=pdfwrite -g${w2px}x${hpx} -c "<</PageOffset [0 0]>> setpagedevice" -f ${f} #[]needs tinkering
-   gs -o ${rf} -sDEVICE=pdfwrite -g${w2px}x${hpx} -c "<</PageOffset [-${w3} 0]>> setpagedevice" -f ${f} #[]needs tinkering
+   gs -o ${lf} -sDEVICE=pdfwrite -g${w2px}x${hpx} -c "<</PageOffset [${lx} ${ly}]>> setpagedevice" -f ${f} 
+   gs -o ${rf} -sDEVICE=pdfwrite -g${w2px}x${hpx} -c "<</PageOffset [-${rx} ${ry}]>> setpagedevice" -f ${f}
   done
 
